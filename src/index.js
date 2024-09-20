c25842bd1b5cbc2084a8915cf9a66630ad538542
PabiBot <mainepaballo3@gmail.com>
2024-09-20 06:30:32 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity"); 
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let data = new date(response.data.time * 1000);
+
 
   cityElement.innerHTML = response.data.city;
 
+  timeElement.innerHTML = formatDate{date};
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperature.innerHTML = Math.round(temperature);
 
 }
 
+function formatDate(date) {
+   let minutes = date.getMinutes();
+   let hours = date.getHours();
+
+   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+   let day = days[date.getDay()];
+
+   if (minutes < 10) {
+    minutes = `0${minutes}`;
+   }
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
 let apiKey = "6acbf29oe0bed88233dce137461c1tf4";
