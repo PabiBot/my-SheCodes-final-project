602b8bd70f4dbd1353d91095d120738b3c02177a
PabiBot <mainepaballo3@gmail.com>
2024-09-21 07:06:24 +0200

    Added API intergration

--- src/index.js
+++ src/index.js
@@ -58,6 +58,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "6acbf29oe0bed88233dce137461c1tf4";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&{unit=metric}`;
@@ -69,32 +76,35 @@ function getForecast(city) {
 
 function displayForecast(response) {
 
-
-
-  let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
   let forecastHTML = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
     forecastHTML =
       forecastHTML +
       `
           <div class="weather-forecast-day"> 
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div class="weather-forecast-icon">
+            <img src="${day.condition.icon_url}" />
+            </div>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-              <strong>15°</strong>
+              <strong>${Math.round(day.temperature.maximum)}°</strong>
               </div>
-              <div class="weather-forecast-temperature">9°</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°</div>
             </div>
           </div>
 `;
-  });
+            }
+          });
 
-  forecastElement.innerHTML = forecastHTML;
-}
+
+let forecastElement = document.querySelector("#forecast");
+   forecastElement.innerHTML = forecastHTML;
+        }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
