0c272bac44d1631a44100c5734d02f05a9019561
PabiBot <mainepaballo3@gmail.com>
2024-09-21 04:33:56 +0200

    inject the forecast HTML from javascript

--- src/index.js
+++ src/index.js
@@ -3,58 +3,89 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity"); 
+  let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let data = new date(response.data.time * 1000);
 
-  let iconElement= document.querySelector {
-    "#icon"
-  }
-  
+  let iconElement = document.querySelector("#icon");
+
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"
           />`;
 
   cityElement.innerHTML = response.data.city;
 
-  timeElement.innerHTML = formatDate{date};
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperature.innerHTML = Math.round(temperature);
-
 }
 
 function formatDate(date) {
-   let minutes = date.getMinutes();
-   let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
 
-   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-   let day = days[date.getDay()];
+  let day = days[date.getDay()];
 
-   if (minutes < 10) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
-   }
+  }
 
   return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
-let apiKey = "6acbf29oe0bed88233dce137461c1tf4";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(refreshWeather); 
-
+  let apiKey = "6acbf29oe0bed88233dce137461c1tf4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  
+
   searchCity(searchInput.value);
+}
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+          <div class="weather-forecast-day"> 
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+              <strong>15°</strong>
+              </div>
+              <div class="weather-forecast-temperature">9°</div>
+            </div>
+          </div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
